f26d30309354f320dfd705189ef914c9087b34bf
Jane <shinigamideico@gmail.com>
2020-11-18 21:52:58 -0500

    Volumes Skeleton

--- RON/Assets/PauseControls.cs
+++ RON/Assets/PauseControls.cs
@@ -40,15 +40,12 @@ public class PauseControls : MonoBehaviour
     }
     public void DeactivatePause(){
         Time.timeScale = 1f; 
-       // Player.SetActive(true);
         PauseScreen.SetActive(false); 
     }
     public void ActivatePause(){
         Time.timeScale = 0f; 
-        //Player.SetActive(false); 
         PauseScreen.SetActive(true); 
         Debug.Log("I get here"); 
         t.text = "Your Stats:\nHealth= " + p.health + "/" +p.maxHealth+ ",   Money= $" + p.money + ",   Speed= " + p.speed + ",   CritChance= " + p.critChance;
-        //PauseScreen.SetActive(true); 
     }
 }
