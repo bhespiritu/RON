79d7a24b1d7e05f029289fb5b3708e476017eaad
Kevin Mulliss <kevinmulliss@Kevins-MacBook-Pro.local>
2020-11-25 22:39:21 -0500

    short descriptions added

--- RON/Assets/Scripts/Items/Passive Items/CritBoost.cs
+++ RON/Assets/Scripts/Items/Passive Items/CritBoost.cs
@@ -4,6 +4,7 @@
     public CritBoost() : base("Thermal Radar", 4, "This thermal apparatus allows you to better aim your shots, increasing your chance to do critical damage.", 50)
     {
         this.critBoost = 0.05f;
+        this.shortDescription = this.itemName + ": Increases chance of doing critical damage on a shot."
     }
     public override void ApplyBonus(Player p)
     {
