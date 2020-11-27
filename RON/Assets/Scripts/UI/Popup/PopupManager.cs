434d817e8d3f46d9c9ce922b4b8f42d6100a96af
bhespiritu <lozman1@elne.org>
2020-11-27 02:06:02 -0500

    popup queue system

--- RON/Assets/Scripts/UI/Popup/PopupManager.cs
+++ RON/Assets/Scripts/UI/Popup/PopupManager.cs
@@ -9,11 +9,22 @@ public class PopupManager : MonoBehaviour
     public Canvas PopupCanvas;
     public Image PopupParent;
     public Image PopupIcon;
+    public RectTransform IconHolder;
     public Text PopupTitle;
     public Text PopupDesc;
 
+    public Sprite testSprite;
+
+    public bool showIcon = true;
+
     public float openTime = 0.2f;
     public float closeTime = 0.2f;
+
+    public bool isOpen { get; }
+
+    private float initialWidth;
+    private Queue<Popup> popupQueue;
+    private bool popupActive = false;
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -32,31 +43,103 @@ public class PopupManager : MonoBehaviour
     private void Start()
     {
         initialWidth = PopupParent.rectTransform.rect.width;
+        popupQueue = new Queue<Popup>();
+        //Sample Usage:
+        //queuePopup(5,"Sample Title", "This is a test message", testSprite);
+        queuePopup(5, "Sample Title", "This is a test message", testSprite);
+        queuePopup(2, "Sample Title2", "This is a test message2");
+        queuePopup(5, "Sample Title3", "This is a test message3", testSprite);
     }
 
-    private int framesSince;
-    private float triggerTime;
+    private void Update()
+    {
+        if (!popupActive)
+        {
+            if (popupQueue.Count > 0)
+            {
+                Popup pop = popupQueue.Dequeue();
 
-    public bool isOpen { get; }
+                PopupTitle.text = pop.title;
+                PopupDesc.text = pop.description;
+                if(pop.icon)
+                {
+                    PopupIcon.sprite = pop.icon;
+                    PopupIcon.gameObject.SetActive(true);
+                    IconHolder.gameObject.SetActive(true);
+                } else
+                {
+                    IconHolder.gameObject.SetActive(false);
 
-    private float initialWidth;
+                }
+               
+                PopupDesc.gameObject.SetActive(!string.IsNullOrEmpty(pop.description));
+                PopupTitle.gameObject.SetActive(!string.IsNullOrEmpty(pop.title));
 
-    public void triggerOpen()
+                StartCoroutine(popUpFor(pop.duration));
+            }
+        }
+    }
+
+    private void triggerOpen()
     {
         StartCoroutine(animateOpen());
     }
 
-    public void triggerClose()
+    private void triggerClose()
     {
         StartCoroutine(animateClose());
     }
 
+    private void showFor(float time)
+    {
+        StartCoroutine(popUpFor(time));
+    }
+
+    public void queuePopup(float time, string title, string message, Sprite icon = null)
+    {
+        Popup pop = new Popup(time, title, message, icon);
+        popupQueue.Enqueue(pop);
+        
+    }
+
+    IEnumerator popUpFor(float time)
+    {
+        popupActive = true;
+        float t = 0.01f;
+        while (t < openTime)
+        {
+            PopupParent.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, EaseIn(t / openTime) * initialWidth);
+            PopupDesc.color = Color.Lerp(Color.white, Color.black, EaseOut(t / openTime));
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        PopupParent.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, initialWidth);
+
+        yield return new WaitForSeconds(time);
+
+        t = 0.01f;
+
+        while (t < closeTime)
+        {
+            PopupParent.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, EaseIn(1 - (t / closeTime)) * initialWidth);
+            PopupDesc.color = Color.Lerp(Color.black, Color.white, EaseOut(t / closeTime));
+            t += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        PopupParent.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0);
+        PopupDesc.gameObject.SetActive(false);
+        PopupTitle.gameObject.SetActive(false);
+        PopupIcon.gameObject.SetActive(false);
+
+        yield return new WaitForEndOfFrame();
+        popupActive = false;
+    }
+
+
     IEnumerator animateOpen()
     {
+        popupActive = true;
         float t = 0.01f;
-        PopupDesc.gameObject.SetActive(true);
-        PopupTitle.gameObject.SetActive(true);
-        PopupIcon.gameObject.SetActive(true);
         while (t < openTime)
         {
             PopupParent.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, EaseIn(t / openTime) * initialWidth);
@@ -70,6 +153,7 @@ public class PopupManager : MonoBehaviour
 
     IEnumerator animateClose()
     {
+        popupActive = false;
         float t = 0.01f;
 
         while (t < closeTime)
@@ -100,6 +184,22 @@ public class PopupManager : MonoBehaviour
     {
         float invT = 1 - t;
         return 1 - invT * invT * invT;
+    }
+
+    private struct Popup
+    {
+        public string title;
+        public string description;
+        public Sprite icon;
+        public float duration;
+        public Popup(float duration = 5, string title = "", string desc = "", Sprite sprite = null)
+        {
+            this.title = title;
+            this.description = desc;
+            this.icon = sprite;
+            this.duration = duration;
+        }
+
     }
 
 }
