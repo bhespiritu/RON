5cd43df750865001e11d18e154c50dfcc2cc1351
Jane <shinigamideico@gmail.com>
2020-12-01 16:08:01 -0500

    CoolDown Implemented

--- RON/Assets/Scripts/Items/SecondaryItem.cs
+++ RON/Assets/Scripts/Items/SecondaryItem.cs
@@ -6,6 +6,8 @@ public class SecondaryItem : Item
     public bool canUse = true;
     public float coolDownAmount = 0f;
     public float coolDownTimer = 0f;
+    public ItemUI thingy = GameObject.Find("ActiveImage").GetComponent<ItemUI>();
+
 
     public SecondaryItem(Player p, string name = "", int id = 0, string description = "This item doesn't do much. It's just a paper weight.", int cost = 0) : base(name, id, description, cost)
     {
@@ -14,10 +16,13 @@ public class SecondaryItem : Item
 
     public virtual void Effect(bool click)
     {
+        
     }
 
     public void CoolDown()
     {
+        
+    
         if (this.coolDownTimer < this.coolDownAmount)
         {
             this.coolDownTimer += Time.deltaTime;
@@ -27,5 +32,6 @@ public class SecondaryItem : Item
             this.coolDownTimer = 0f;
             this.canUse = true;
         }
+
     }
 }
