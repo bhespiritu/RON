413336e4905cc7813ebf12e4583fa29627b0d68d
Kevin Mulliss <kevinmulliss@d-172-25-98-129.dhcp.virginia.edu>
2020-11-30 22:02:34 -0500

    item rebalancing

--- RON/Assets/Scripts/Items/Passive Items/DamageBoost.cs
+++ RON/Assets/Scripts/Items/Passive Items/DamageBoost.cs
@@ -2,14 +2,15 @@
 
 public class DamageBoost : PassiveItem
 {
-    public float damageScale;
+    public static int count = 0;
+
     public DamageBoost() : base("Bigger Bullets", 1, "These bullets will make you do more damage. Bigger is better, right?", 30)
     {
         this.shortDescription = this.itemName + ": Increases damage output.";
-        this.damageScale = 1.05f;
     }
     public override void ApplyBonus(Player p)
     {
-        p.damageMultiplier *= this.damageScale;
+        DamageBoost.count++;
+        p.damageMultiplier = p.baseDamageMultiplier + DamageBoost.count / 7f;
     }
 }
