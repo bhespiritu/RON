747628fcebe103d9ffc333f2f1cdaf9656097d2c
ash2taf <ash2taf@virginia.edu>
2020-12-03 23:30:46 -0500

    basic killer backup

---
+++ RON/Assets/Backup_Killer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backup_Killer : MonoBehaviour
+{
+    GameObject player;
+    Player playerInf;
+    public float minHt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerInf = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.y < minHt){
+            playerInf.health = 0;
+        }
+    }
+}
